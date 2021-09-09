abe8a61fe87dbc4b818ab58e0445f4a92bb37d83
Emiliano Cocom <joseemilianococom@gmail.com>
2021-09-09 01:09:29 -0500

    Instale laravel passport, estan listas los modelos y controllers

---
+++ app/Models/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Traits\ApiTrait;
+
+class Post extends Model
+{
+    use HasFactory, ApiTrait;
+
+    protected $fillable = ['name', 'slug', 'extract', 'body', 'status', 'category_id', 'user_id'];
+
+    const BORRADOR = 1;
+    const PUBLICADO = 2;
+
+    //Relacion uno a muchos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relacion muchos a muchos
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relacion uno a muchos polimorfica
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
+
---
+++ app/Models/Tag.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+use App\Traits\ApiTrait;
+
+class Tag extends Model
+{
+    use HasFactory, ApiTrait;
+
+    //Relacion muchos a muchos
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+}
