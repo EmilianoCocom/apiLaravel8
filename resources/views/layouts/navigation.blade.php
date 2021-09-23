d68eeed4c609399b9c1a7b9c6094b141b0ee03cb
Emiliano Cocom <joseemilianococom@gmail.com>
2021-09-23 10:59:05 -0500

    add clients form

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,6 +34,11 @@
                     </x-slot>
 
                     <x-slot name="content">
+
+                        <x-dropdown-link :href="route('clients.index')">
+                            Clientes
+                        </x-dropdown-link>
+
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
