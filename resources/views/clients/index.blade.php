6c299fc2c77e76a3259280784a9a0e330f721975
Emiliano Cocom <joseemilianococom@gmail.com>
2021-10-02 13:40:32 -0500

    edit clients

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -5,100 +5,163 @@
         </h2>
     </x-slot>
 
-    <x-container id="app" class="py-8">
-        <x-form-section class="mb-12">
+    <div id="app">
 
+        <x-container class="py-8">
+
+            {{-- Crear clientes --}}
+            <x-form-section class="mb-12">
+
+                <x-slot name="title">
+                    Crear un nuevo cliente
+                </x-slot>
+
+                <x-slot name="description">
+                    Ingrese los datos solicitados para poder crear un nuevo cliente
+                </x-slot>
+                
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-4">
+                        
+                        <div v-if="createForm.errors.length > 0"
+                        class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+                            <strong class="font-bold">Whoops!</strong>
+                            <span>¡Algo salió mal!</span>
+
+                            <ul>
+                                <li v-for="error in createForm.errors">
+                                    @{{error}}
+                                </li>
+                            </ul>
+                        </div>
+
+                        <x-label>
+                            Nombre
+                        </x-label>
+
+                        <x-input v-model="createForm.name" type="text" class="w-full mt-1" />
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label>
+                            URL de redirección
+                        </x-label>
+
+                        <x-input v-model="createForm.redirect" type="text" class="w-full mt-1" />
+                    </div>
+                </div>
+
+                <x-slot name="actions">
+                    <x-button v-on:click="store" v-bind:disabled="createForm.disabled">
+                        Crear
+                    </x-button>
+                </x-slot>
+
+            </x-form-section>
+
+            {{-- Mostrar clientes --}}
+            <x-form-section v-if="clients.length > 0">
+
+                <x-slot name="title">
+                    Lista de clientes
+                </x-slot>
+
+                <x-slot name="description">
+                    Aquí podrás encontrar todos los clientes que has agregado
+                </x-slot>
+                
+                <div>
+
+                    <table class="text-gray-600">
+                        <thead class="border-b border-gray-300">
+                            <tr class="text-left">
+                                <th class="py-2 w-full">Nombre</th>
+                                <th class="py-2 ">Acción</th>
+                            </tr>
+                        </thead>
+
+                        <tbody class="divide-y divide-gray-300">
+                            <tr v-for="client in clients">
+                                <td class="py-2">
+                                    @{{client.name}}
+                                </td>
+
+                                <td class="flex divide-x divide-gray-300 py-2">
+                                    <a v-on:click="edit(client)" class="pr-2 hover:text-blue-600 font-semibold cursor-pointer">
+                                        Editar
+                                    </a>
+
+                                    <a class="pl-2 hover:text-red-600 font-semibold cursor-pointer"
+                                        v-on:click="destroy(client)">
+                                        Eliminar
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+            </x-form-section>
+
+        </x-container>
+
+        {{-- Modal --}}
+        <x-dialog-modal modal="editForm.open">
             <x-slot name="title">
-                Crear un nuevo cliente
+                Editar cliente
             </x-slot>
 
-            <x-slot name="description">
-                Ingrese los datos solicitados para poder crear un nuevo cliente
-            </x-slot>
-            
-            <div class="grid grid-cols-6 gap-6">
-                <div class="col-span-6 sm:col-span-4">
-                    
-                    <div v-if="createForm.errors.length > 0"
-                    class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+            <x-slot name="content">
+                <div class="space-y-6">
+
+                    <div v-if="editForm.errors.length > 0"
+                        class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
                         <strong class="font-bold">Whoops!</strong>
                         <span>¡Algo salió mal!</span>
 
                         <ul>
-                            <li v-for="error in createForm.errors">
+                            <li v-for="error in editForm.errors">
                                 @{{error}}
                             </li>
                         </ul>
                     </div>
 
-                    <x-label>
-                        Nombre
-                    </x-label>
+                    <div class="">
+                        
+                        
 
-                    <x-input v-model="createForm.name" type="text" class="w-full mt-1" />
+                        <x-label>
+                            Nombre
+                        </x-label>
+
+                        <x-input v-model="editForm.name" type="text" class="w-full mt-1" />
+                    </div>
+
+                    <div class="">
+                        <x-label>
+                            URL de redirección
+                        </x-label>
+
+                        <x-input v-model="editForm.redirect" type="text" class="w-full mt-1" />
+                    </div>
                 </div>
-
-                <div class="col-span-6 sm:col-span-4">
-                    <x-label>
-                        URL de redirección
-                    </x-label>
-
-                    <x-input v-model="createForm.redirect" type="text" class="w-full mt-1" />
-                </div>
-            </div>
-
-            <x-slot name="actions">
-                <x-button v-on:click="store" v-bind:disabled="createForm.disabled">
-                    Crear
-                </x-button>
             </x-slot>
 
-        </x-form-section>
+            <x-slot name="footer">
+                <button v-on:click="update()" v-bind:disabled="editForm.disabled" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm disabled:opacity-50">
+                    Actualizar
+                </button>
 
-        <x-form-section v-if="clients.length > 0">
-
-            <x-slot name="title">
-                Lista de clientes
+                <button v-on:click="editForm.open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                    Cancelar
+                </button>
             </x-slot>
+        </x-dialog-modal>
 
-            <x-slot name="description">
-                Aquí podrás encontrar todos los clientes que has agregado
-            </x-slot>
-            
-            <div>
-
-                <table class="text-gray-600">
-                    <thead class="border-b border-gray-300">
-                        <tr class="text-left">
-                            <th class="py-2 w-full">Nombre</th>
-                            <th class="py-2 ">Acción</th>
-                        </tr>
-                    </thead>
-
-                    <tbody class="divide-y divide-gray-300">
-                        <tr v-for="client in clients">
-                            <td class="py-2">
-                                @{{client.name}}
-                            </td>
-
-                            <td class="flex divide-x divide-gray-300 py-2">
-                                <a class="pr-2 hover:text-blue-600 font-semibold cursor-pointer">
-                                    Editar
-                                </a>
-
-                                <a class="pl-2 hover:text-red-600 font-semibold cursor-pointer">
-                                    Eliminar
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-
-                </table>
-
-            </div>
-
-        </x-form-section>
-    </x-container>
+    </div>
+  
 
     @push('js')
 
@@ -113,6 +176,15 @@
                         errors: [],
                         name: null,
                         redirect: null,
+                    },
+                    editForm:{
+                        open: false,
+                        errors: [],
+                        disabled: false,
+                        errors: [],
+                        id: null,
+                        name: null,
+                        redirect: null,
                     }
 
                 },
@@ -121,12 +193,14 @@
                 },
 
                 methods:{
+
                     getClients(){
                         axios.get('/oauth/clients')
                         .then(response => {
                             this.clients = response.data
                         });
                     },
+
                     store(){
 
                         this.createForm.disabled = true;
@@ -135,6 +209,7 @@
                             .then(response => {
                                 this.createForm.name = null;
                                 this.createForm.redirect = null;
+                                this.createForm.errors = [];
                                 Swal.fire(
                                     'Creado con éxito!',
                                     'El cliente se creó satisfactoriamente.',
@@ -153,7 +228,69 @@
 
                             })
                     
-                    }
+                    },
+
+                    edit(client){
+                        this.editForm.open = true;
+                        this.editForm.errors = [];
+                        this.editForm.id = client.id;
+                        this.editForm.name = client.name;
+                        this.editForm.redirect = client.redirect;
+                    },
+
+                    update(){
+                        this.editForm.disabled = true;
+
+                        axios.put('/oauth/clients/' + this.editForm.id, this.editForm)
+                            .then(response => {
+                                this.editForm.open = false;
+                                this.editForm.disabled = false;
+                                this.editForm.name = null;
+                                this.editForm.redirect = null;
+                                this.editForm.errors = [];
+                                Swal.fire(
+                                    'Actualizado con éxito!',
+                                    'El cliente se actualizó satisfactoriamente.',
+                                    'success'
+                                )
+
+                                this.getClients();
+
+                            }).catch(error => {
+
+                                this.editForm.errors = _.flatten(_.toArray(error.response.data.errors));
+
+                                this.editForm.disabled = false;
+
+                        })
+                    },
+
+                    destroy(client){
+                        Swal.fire({
+                            title: 'Are you sure?',
+                            text: "You won't be able to revert this!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Yes, delete it!'
+                            }).then((result) => {
+                            if (result.isConfirmed) {
+
+                                axios.delete('/oauth/clients/' + client.id)
+                                    .then(response => {
+                                        this.getClients();
+                                    });
+
+                                Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                                )
+                            }
+                        })
+
+                    },
                 }
             });
 
